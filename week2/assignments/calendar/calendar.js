d85256d66b8a1dcbf85548819bf7b82a7c5e8417
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-08-14 14:11:59 -0500

    not complete

--- week2/assignments/calendar/calendar.js
+++ week2/assignments/calendar/calendar.js
@@ -23,23 +23,6 @@ switch (month) {
     default:
         days = 31
 }
-document.getElementById("selectMonth")
-{<option value="0">Months</option>
-<option value="1">January</option>
-<option value="2">February</option>
-<option value="3">March</option>
-<option value="4">April</option>
-<option value="5">May</option>
-<option value="6">June</option>
-<option value="7">July</option>
-<option value="8">August</option>
-<option value="9">September</option>
-<option value="10">October</option>
-<option value="11">November</option>
-<option value="12">December</option>
-
-}
-
 let dt = 0
 do {
     dt++
@@ -47,6 +30,7 @@ do {
 } while ( dt < days)
 
 const monthName = today.toLocaleDateString('default', {month:'long'})
+
 year = today.getFullYear()
 document.querySelector('.calendarTitle').innerText = `${monthName} ${year}`
 
