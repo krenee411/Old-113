72d166c9a250729852201568488d6977c905edfd
krenee411 <kelci.morgan@bryanuniversity.edu>
2021-08-25 19:14:56 -0500

    added

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -3,17 +3,32 @@ const lang = 'JavaScript'
 // Create an event listener for the submit button that adds all 'input' elements to 
 // a single array using the spread operator. Call the chkLang() function, passing in 
 // the array as an argument.
-
+document.getElementById('submit').addEventListener('click',chkLang)
 
 function chkLang(langs) {
     let result = false
-
+    var inputs = document.getElementsByTagName('input')
+    //console.log(inputs)
+    var items= []
+    for(i= 0; i<inputs.length; i++){
+       var area = inputs[i].value.split(',') 
+       items= [...items,...area]
+     
+    }
+      //  console.log(items)
+     items.find(function(getIt){
+     if(items === `${lang}`){
+         return result 
+     }
+    })
+    
+ 
     // use an array method to check whether the user included 'JavaScript' in their
     // list of languages
-
     let obj = document.querySelector('#TestResult')
     if (result) 
         obj.innerText = `Congratulations!\nYou know ${lang}.`
     else
         obj.innerText = `Sorry,\nyou don't know ${lang}.`
+ 
 }
--- week4/assignments/scavenger/scavenger.js
+++ week4/assignments/scavenger/scavenger.js
@@ -3,10 +3,26 @@
 // Modify the combineLists() function to create arrays from each scavenger hunt element in the scavenger.html document. 
 // Then use the spread operator to combine these arrays into a single array. 
 // Sort that array and display it in the 'AllItems' element on the scavenger.html document.
+document.getElementById('submit').addEventListener('click',combineLists)
 
 function combineLists() {
+ var textareas = document.getElementsByTagName('textarea')
+ console.log(textareas)
+ var items= []
+ for(i= 0; i<textareas.length; i++){
+    var oneTextArea = textareas[i].value.split(',') 
+   // console.log(oneTextArea)
+    items= [...items,...oneTextArea]
+ }
+ items.sort()
+console.log(items)
+items.forEach(function(getItems) {
+    let allItems = document.createElement('p')
+    allItems.setAttribute('id', 'allItemsList')
+    allItems.textContent += getItems
+    document.getElementById('AllItems').appendChild(allItems) 
+});
 }
-
 // The following code is a bonus addition that facilitates data entry on the scavenger.html document.
 // If you first click in one of the textareas, you can then click scavenger hunt items in the unordered
 // list. The value of that will be added to the appropriate textarea along with any necessary commas.
